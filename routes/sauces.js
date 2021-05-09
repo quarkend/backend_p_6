c501195fad9880d5b348636770b2a68cf479c9f2
quarkend <39971072+quarkend@users.noreply.github.com>
2021-05-09 17:40:20 +0200

    Update sauces.js
    Supp commentaires

--- routes/sauces.js
+++ routes/sauces.js
@@ -1,5 +1,3 @@
-/***********changement de stuff par sauces*********************** */
-
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
@@ -7,18 +5,11 @@ const stuffCtrl = require('../controllers/sauces');
 const multer = require('../middleware/multer-config');
 
 router.post('/', auth, multer, stuffCtrl.createThing);
-/* la route ou le n point sur notre api*/
 router.get('/', auth, stuffCtrl.getAllThings);
 router.get('/:id', auth, stuffCtrl.getOneThing);
-
-/* UNE ROUTE PUT pour modifier une saucve ds la base de donnee(le prem arg _id objet de comparaison 
-    le 2 arg cest la nouvelle version de l objet utilise le spride oppertar... , corespond*/
 router.put('/:id', auth, multer, stuffCtrl.modifyThing);
-
-
 /* delete le dernier crud de api*/
 router.delete('/:id', auth, stuffCtrl.deleteThing);
-
 router.post('/:id/like', auth, stuffCtrl.likeSauces);
 
 module.exports = router;
